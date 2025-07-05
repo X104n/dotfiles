522d55e7af7b4b0b02199c797173f231b13d99d5
X104n <github.freeware171@passmail.net>
2025-07-05 15:42:40 +0200

    mason lsp moved to github organisation
    Look into what need to be done to make the lsp work.

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,21 @@
+return {
+    {
+        "mason-org/mason.nvim",
+        opts = {}
+    },
+    {
+        "mason-org/mason-lspconfig.nvim",
+        opts = {},
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({})
+        end
+    }
+}
